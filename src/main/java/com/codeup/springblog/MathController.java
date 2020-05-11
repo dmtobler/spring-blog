b0dc22e8f4a60badb4a02fad3ae65606cd6a61ab
Danielle Tobler <danielle.tobler210@gmail.com>
2020-05-11 11:35:31 -0500

    Fixed classes to be Controllers, changed output to be Strings

---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,31 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable double num1, @PathVariable double num2) {
+        return num1 + " plus " + num2 + " is " + (num1 + num2) + "!";
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable double num1, @PathVariable double num2) {
+        return num2 + " minus " + num1 + " is " + (num2 - num1) + "!";
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable double num1, @PathVariable double num2) {
+        return num1 + " multiplied by " + num2 + " is " + (num1 * num2) + "!";
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable double num1, @PathVariable double num2) {
+        return num1 + " divided by " + num2 + " is " + (num1 / num2) + "!";
+    }
+}
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,13 +1,17 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class PostController {
 
 //    Posts index page
 
-    @GetMapping(path = "/posts")
+    @GetMapping("/posts")
     @ResponseBody
     public String returnAllPosts() {
         return "Posts index page";
