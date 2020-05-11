ae4297f3c59e41ea1336d0183d6834072294c322
Danielle Tobler <danielle.tobler210@gmail.com>
2020-05-11 15:58:43 -0500

    Added partials

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.codeup.springblog;
+package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +11,10 @@ public class HomeController {
     @ResponseBody
     public String index() {
         return "This is the landing page!";
+    }
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
 }
