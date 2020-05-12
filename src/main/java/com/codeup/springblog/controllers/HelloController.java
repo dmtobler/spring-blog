d2b829f5db7246988ca5b11e6e5c55feb12c42fd
Danielle Tobler <danielle.tobler210@gmail.com>
2020-05-11 21:01:25 -0500

    Did the dice exercise

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -2,13 +2,16 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring!";
+    }
 
     @GetMapping("/hello/{name}")
     public String sayHello(@PathVariable String name, Model model) {
--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,8 +1,31 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RollDiceController {
 
+    @GetMapping("/roll-dice")
+    public String showRollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{num}")
+    public String rollDice(@PathVariable int num, Model model) {
+        int randomNum = (int) (1 + (Math.random() * 6));
+        model.addAttribute("num", "Your number: " + num);
+        model.addAttribute("randomNum", "Dice roll: " + randomNum);
+
+        if(num != 0) {
+            if(randomNum == num) {
+                model.addAttribute("correctGuess", true);
+            } else {
+                model.addAttribute("correctGuess", false);
+            }
+        }
+        return "roll-dice";
+    }
 }
